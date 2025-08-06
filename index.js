0e4a5808a0e088908aedd910aae3f5c7d649ee02
João Victor <victorferreiraaraujo9@gmail.com>
2025-08-05 22:52:36 -0300

    feat: lembretes automáticos via whats

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const axios = require('axios');
+
+async function sendMessage(message) {
+    const url = 'https://api.gupshup.io/sm/api/v1/msg';
+
+    try {
+        const res = await axios.post(
+            url,
+            new URLSearchParams({
+                channel: 'whatsapp',
+                source: '+917834811114',
+                destination: process.env.DESTINY,
+                message: message,
+                'src.name': 'autoreminder'
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'apikey': process.env.GUPSHUP_API_KEY
+                }
+            }
+        );
+
+        console.log('Mensagem enviada:', res.data);
+    } catch (err) {
+        console.error('Erro ao enviar mensagem:', err.response?.data || err.message);
+    }
+}
+
+function checkReminder() {
+    const today = new Date();
+    const dayWeek =  today.getDay(); 
+
+    // lembrete toda sexta
+    if (dayWeek === 5) {
+        sendMessage('Lembrete: PEDIDO BOLOOOO CAÇAROLACAÇAROLA 🥘');
+    }
+
+    // lista exames e consultas (ano, mes-1, dia)
+    const exams = [
+        new Date(2025, 7, 13), // 13 de agosto 2025
+    ];
+
+    exams.forEach(data => {
+        if (
+            data.getDate() === today.getDate() &&
+            data.getMonth() === today.getMonth() &&
+            data.getFullYear() === today.getFullYear()
+        ) {
+            sendMessage('Lembrete: EXAME MARCADO PARA AMANHÃ 🏥');
+        }
+    });
+}
+
+checkReminder();
